b2b6ba3903f044cfe0b091318616f19e03a67c04
bissbotch <64175657+bissbotch@users.noreply.github.com>
2021-06-16 01:37:25 +0100

    Update account.rs

--- src/account.rs
+++ src/account.rs
@@ -1,4 +1,10 @@
 use std::io;
+use std::fs::File;
+
+struct User {
+    name: String,
+    pass: String,
+}
 
 pub fn login() {
     println!("Enter your username: \n");
@@ -14,16 +20,30 @@ pub fn login() {
         .expect("Error reading password");
 }
 
-pub fn register() {
-    println!("Enter your username: \n");
-    let mut username = String::new();
-    io::stdin()
-        .read_line(&mut username)
-        .expect("Error reading username");
 
-    println!("Enter your password: \n");
-    let mut password = String::new();
-    io::stdin()
-        .read_line(&mut password)
-        .expect("Error reading password");
+impl User {
+    pub fn register() {
+        println!("Enter your username: \n");
+        let mut username = String::new();
+        io::stdin()
+            .read_line(&mut username)
+            .expect("Error reading username");
+    
+        println!("Enter your password: \n");
+        let mut password = String::new();
+        io::stdin()
+            .read_line(&mut password)
+            .expect("Error reading password");
+
+    }
+}
+
+pub fn checkForAccount() {
+    let file = File::open("accounts.txt");
+
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => panic!("Error opening the file: {:?}", error),
+    };
+    
 }
